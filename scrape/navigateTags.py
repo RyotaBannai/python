2b746d493ab4e5a6d47574421127f4d2aba8ac8e
RyotaBannai <ryotala0528@gmail.com>
2020-02-23 22:24:42 +0900

    learnt basic usage of beautiful soup.

---
+++ scrape/navigateTags.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError 
+from urllib.error import URLError
+from bs4 import BeautifulSoup as BS
+from scrapetest import *
+import re
+  
+def getChildren(html):
+  try:
+    html = BS(html, 'html5lib')
+    return html.find('table', {'id':'giftList'}) # 可能な限り、属性を付けてSpecificに.
+  except AttributeError:
+    return None
+
+def getGiftImg(html):
+  try:
+    html = BS(html, 'html5lib')
+    return html.find_all('img', 
+       {'src': re.compile(r'\.\./img/gifts/img.*\.jpg')}) # . matches . as character.
+  except AttributeError:
+    return None
+
+if __name__ == "__main__":
+  url = 'http://www.pythonscraping.com/pages/page3.html'
+  html = getHtml(url)
+  #tags = getChildren(html)
+  tags = getGiftImg(html)
+
+  if html and tags and False:
+    for child in tags.children: 
+      # find_all だとリストが返させれるから、.chidrenメソッドをそのまま呼び出せないことに注意.
+      print(child)
+
+    print(len((list(tags.children)))) #>>> 2
+    print(len(list(tags.descendants))) #>>> 87
+
+  if html and tags and False:
+    for bro in tags.tr.next_siblings:
+      print(bro) 
+      # trタグを前から順に取得. 上のは tbodyを一気に取得.　
+      # タイトル行はnextからなので含まれない.
+
+  if html and tags:
+    for img in tags:
+      print(img.attrs['src']) # access to attributes
+  else:
+    print('An error has occured')
+  
--- scrape/scrapetest.py
+++ scrape/scrapetest.py
@@ -4,17 +4,20 @@ from urllib.error import URLError # for can't even reach out to page
 from bs4 import BeautifulSoup as BS
 import sys
 
-def getHtml(url: str):
+__all__ = ['getHtml']
+
+def getHtml(url):
   """
     check out whether url is available or not.
     if it's aivailable, then check if tag exists.
   """
-  try: 
-	  html = urlopen(url)
+  try:
+    return urlopen(url)
   except (HTTPError, URLError):
-	  return None
-	  #return nullm break, or run Plan B
-  
+    return None
+    #return nullm break, or run Plan B
+
+def extractTag(html): 
   try:
     html = BS(html, 'html5lib')
     return html.div
@@ -23,11 +26,28 @@ def getHtml(url: str):
     # return sys.exc_info()
     # return isinstance(tag, Exception)
 
+def extractTagByClass(html): 
+  try:
+    html = BS(html, 'html5lib')
+    #return html.find_all('span', {'class': 'green'}) # get by attributes.
+    #return html.find_all('', {'class': {'green', 'red'}}) 
+    return html.find_all(class_ = ['green', 'red'])  # get by keywords
+
+    # keyword => AND filter ex, .find_all(id='red', class_='text')
+    # attribute => OR filter ex, .find_all('', {'id': 'red', 'class': 'text') 
+  except AttributeError:
+    return None
+    
 if __name__ == "__main__":
-  url = 'http://pythonscraping.com/pages/page1.html'
-  tag = getHtml(url)
-  if tag is not None:
-    print(tag)
+  #url = 'http://pythonscraping.com/pages/page1.html'
+  url = 'https://www.pythonscraping.com/pages/warandpeace.html'
+  html = getHtml(url)
+  #tags = extractTag(html)
+  tags = extractTagByClass(html)
+  if html and tags: # If either of them returns None, then it's an error.
+    #print(tags)
+    for tag in tags:
+      print(tag.get_text(), end=", ")
   else:
     print('An error has occured')
   
