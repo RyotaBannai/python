0c38381ff0d373661965454be2b777b6ffd26b66
RyotaBannai <ryotala0528@gmail.com>
2020-03-11 00:03:14 +0900

    abuse of define attributes inside fn.

--- advanced_usage/property_in_func.py
+++ advanced_usage/property_in_func.py
@@ -9,6 +9,17 @@ class Myclass():
   def my_func(self, arg1, arg2):
     print('im my_func! you passed args {}, {}'.format(arg1, arg2))
 
+# abuse of using attributes. usually fn can't have attr this way.
+def FakeObj():
+  def test():
+    print("foo")
+  FakeObj.test = test
+  return FakeObj
+
+
 if __name__ == "__main__":
   myclass = Myclass()
-  print(myclass.my_func.is_webmethod)
+  # print(myclass.my_func.is_webmethod)
+
+  x = FakeObj()
+  x.test()
