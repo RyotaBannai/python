d9a113fa1097231da02a8364669096a073066658
RyotaBannai <ryotala0528@gmail.com>
2020-03-13 23:29:02 +0900

    lerant advanced usages of dict combination.

---
+++ advanced_usage/dict_usage.py
@@ -0,0 +1,74 @@
+# how to marge dict
+from copy import deepcopy
+
+
+def merge_two_dict(a: dict, b: dict):
+    x = a.copy()
+    x.update(b)
+    return x
+
+
+def dict_of_str_merge(x, y):
+    z = {}
+    overlapping_keys = x.keys() & y.keys()
+    for key in overlapping_keys:
+        z[key] = [x[key], y[key]]
+    for key in x.keys() - overlapping_keys:
+        z[key] = deepcopy(x[key])
+    for key in y.keys() - overlapping_keys:
+        z[key] = deepcopy(y[key])
+    return z
+
+
+def dict_of_dicts_merge(x, y):
+    z = {}
+
+    if isinstance(x, str) | isinstance(y, str):
+        return [x, y]
+
+    overlapping_keys = x.keys() & y.keys()
+    for key in overlapping_keys:
+        z[key] = dict_of_dicts_merge(x[key], y[key])
+    for key in x.keys() - overlapping_keys:
+        z[key] = deepcopy(x[key])
+    for key in y.keys() - overlapping_keys:
+        z[key] = deepcopy(y[key])
+    return z
+
+
+if __name__ == "__main__":
+    # the same keys aren't allowed.
+    x = {'0000': 'Ryota', '0001': 'Taro'}
+    y = {'1000': 'Mark', '1001': 'Zack'}
+    # duplidated items will be removed without addional codes.
+    duplicated_items = {'1002': 'Mark', '1001': 'Zack'}
+
+    z = {**x, **y, **duplicated_items}
+
+    merged = merge_two_dict(x, y)
+
+    # Python 3.9 or greater version
+    # z = x | y # means merging two dicts!!
+
+    # dict items is like set. so union results in combining to dits.
+    new_dict = dict(x.items() | y.items())
+
+    # by using dict constructor: not recommended way to combine.
+    new_dict = dict(x, **y)
+
+    new_items = {'0000': 'Bannai', '0002': 'Stanford'}
+    merged_same_keys = dict_of_str_merge(x, new_items)
+    # print(merged_same_keys)
+
+    mutiple_layered_x = {'a': {1: {}, 2: {}}, 'b': {2: {3: 'Hi'}}}
+    mutiple_layered_y = {'b': {10: 'haha', 2: {3: 'Hello'}}, 'c': {11: {}}}
+
+    merged_same_keys2 = dict_of_dicts_merge(
+        mutiple_layered_x, mutiple_layered_y)
+    # print(merged_same_keys2)
+
+    # the same keys are overwritten by later defined ones
+    have_the_same_keys = {'a': {1: 'Some', 2: 'thing'},
+                          'a': {3: 'any', 4: 'thing'}
+                          }
+    print(have_the_same_keys['a'])
--- advanced_usage/property_in_func.py
+++ advanced_usage/property_in_func.py
@@ -3,39 +3,45 @@ import time
 
 # decorator should come before it's called.
 def webmethod(fn):
-  fn.is_webmethod = "hahaha"
-  return fn
+    fn.is_webmethod = "hahaha"
+    return fn
+
+
 class Myclass():
-  @webmethod 
-  def my_func(self, arg1, arg2):
-    print('im my_func! you passed args {}, {}'.format(arg1, arg2))
+    @webmethod
+    def my_func(self, arg1, arg2):
+        print('im my_func! you passed args {}, {}'.format(arg1, arg2))
 
 
 def print_separator(fn):
-  def my_print(rng: int = 1, target_fn = fn):
-    print('the target function <{}> is being called...'.format(target_fn.__name__))
-    target_fn(rng)
-    print('\nthe target function has finished running...')
-  return my_print
+    def my_print(rng: int = 1, target_fn=fn):
+        print('the target function <{}> is being called...'.format(
+            target_fn.__name__))
+        target_fn(rng)
+        print('\nthe target function has finished running...')
+    return my_print
+
 
 @print_separator
 def my_nothn_special_fn(rng):
-  for n in range(rng):
-    print(n, end='', flush=True)
-    time.sleep(1)
+    for n in range(rng):
+        print(n, end='', flush=True)
+        time.sleep(1)
 
 # abuse of using attributes. usually fn can't have attr this way.
+
+
 def FakeObj():
-  def test():
-    print("foo")
-  FakeObj.test = test
-  return FakeObj
+    def test():
+        print("foo")
+    FakeObj.test = test
+    return FakeObj
 
 
 if __name__ == "__main__":
-  myclass = Myclass()
-  # print(myclass.my_func.is_webmethod)
+    myclass = Myclass()
+    # print(myclass.my_func.is_webmethod)
 
-  # x = FakeObj()
-  # x.test()
-  my_nothn_special_fn(2)
+    # x = FakeObj()
+    # x.test()
+    my_nothn_special_fn(2)
