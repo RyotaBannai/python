40497b329a993f6413564564783aad1ffc8f6df5
RyotaBannai <ryotala0528@gmail.com>
2020-03-17 00:04:41 +0900

    Collections module. you should check this out.

--- buildin_functions/bun_list_none_1.py
+++ buildin_functions/bun_list_none_1.py
@@ -3,4 +3,8 @@ import contextlib
 
 if __name__ == "__main__":
     mylist = ['apple', 'microsoft', 'google', 'apple']
-    Counter(mylist)
+    myfreq_mentioned_words = Counter(mylist)
+    print(myfreq_mentioned_words)
+    to_dict = myfreq_mentioned_words.items()
+    print(to_dict)
+    # 記事に出現する単語の頻度を大きい順に並べるなどの処理に向いている.
